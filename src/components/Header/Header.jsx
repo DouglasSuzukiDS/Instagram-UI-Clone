fb26c5339db372a97cf0462c3d6c6caddd022405
Doug <kaido19@outlook.com>
2022-07-29 22:12:30 -0300

    Adjustments in Home and creating Direct Page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,9 @@
 import Logo from '../../assets/Logos/InstaLogo.svg'
-import Home from '../../assets/Icons/Home-fill.svg'
-import Messenger from '../../assets/Icons/Messenger.svg'
-import NewPosts from '../../assets/Icons/NewPosts.svg'
-import FindPeople from '../../assets/Icons/FindPeople.svg'
-import Like from '../../assets/Icons/Like.svg'
-import User from '../../assets/Images/Luffy.png'
+
 import { Link } from 'react-router-dom'
 
-export default function Header() {
-   return (  
+export default function Header(props) {
+   return (
       <>
          <header className="flex container">
             <main className='flex sbt headerContainer'>
@@ -17,32 +12,32 @@ export default function Header() {
                </div>
 
                <div className='mr-1'>
-                  <input type="text" placeholder='Pesquisar' className='border'/>
+                  <input type="text" placeholder='Pesquisar' className='border' />
                </div>
 
                <div className=''>
                   <Link to='/' className='ml-2'>
-                     <img src={Home} alt="Home" />
+                     <img src={props.homeIcon} alt="Home" />
                   </Link>
 
-                  <Link to='/' className='ml-2'>
-                     <img src={Messenger} alt="Mensagens no Direct" />
+                  <Link to='/direct' className='ml-2'>
+                     <img src={props.messenger} alt="Mensagens no Direct" />
                   </Link>
 
-                  <Link to='/' className='ml-2'>
-                     <img src={NewPosts} alt="Novos Posts" />
+                  <Link to='/newPosts' className='ml-2'>
+                     <img src={props.newPosts} alt="Novos Posts" />
                   </Link>
 
-                  <Link to='/' className='ml-2'>
-                     <img src={FindPeople} alt="Procurar" />
+                  <Link to='/findPeople' className='ml-2'>
+                     <img src={props.findPeople} alt="Procurar" />
                   </Link>
 
-                  <Link to='/' className='ml-2'>
-                     <img src={Like} alt="Notificações" />
+                  <Link to='/like' className='ml-2'>
+                     <img src={props.like} alt="Notificações" />
                   </Link>
 
-                  <Link to='/' className='ml-2'>
-                     <img src={User} alt="Configurações de Usuário" className='headerImg'/>
+                  <Link to='/user' className='ml-2'>
+                     <img src={props.user} alt="Configurações de Usuário" className='headerImg' />
                   </Link>
                </div>
             </main>
@@ -50,3 +45,10 @@ export default function Header() {
       </>
    )
 }
+
+/* // Example for Import Header Component
+   <Header homeIcon={HomeFill} 
+      messenger={Messenger} newPosts={NewPosts} 
+      findPeople={FindPeople} like={Like} user={User}
+   />
+*/
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,58 +1,39 @@
 import Header from "../../components/Header/Header";
-import SharePosts from '../../assets/Icons/SharePosts.svg'
-import UserArrow from '../../assets/Icons/UserArrow.svg' 
-import NewMessage from '../../assets/Icons/NewMessages.svg' 
-import { Link } from "react-router-dom";
-import UserConversation from "../../components/UserConversation/UserConversation"
-import Law from '../../assets/Images/Law.png'
-import Kaido from '../../assets/Images/Kaido.png'
-import Roger from '../../assets/Images/Roger.png'
+
+import HomeFill from '../../assets/Icons/Home-fill.svg'
+import Messenger from '../../assets/Icons/Messenger.svg'
+import NewPosts from '../../assets/Icons/NewPosts.svg'
+import FindPeople from '../../assets/Icons/FindPeople.svg'
+import Like from '../../assets/Icons/Like.svg'
+import User from '../../assets/Images/Luffy.png'
 
 export default function Home() {
    return(
       <>
-         <Header />
+         <Header homeIcon={HomeFill} 
+            messenger={Messenger} newPosts={NewPosts} 
+            findPeople={FindPeople} like={Like} user={User}
+         />
          
-         <main className="containerHome my-3 flex sbt">
-            <section className="sectionPeople border">
-               <div className="userHome flex sbt py-2 px-4">
-                  <div className="f">
-                     <span className="userNameMsg">MonkeyDLuffy <img src={UserArrow} alt="Seta para Baixo" className="arrowUser ml-1" fill='#000'/></span>
+         <div className="containerHome border2">
+            <main className="mainHome f sbt mt-3 border4">
+               <section className="activitiesFriendsHome border3">
+                  <div className="statusFriendsHome py-1 px-2 mb-2">
+                     <div className="userNews">
+                        <img src={Like} alt="" />
+                     </div> 
                   </div>
-                  <div>
-                     <span>
-                        <img src={NewMessage} alt="Nova Mensagem" />
-                     </span>
+
+                  <div className="activityFriendHome py-1 px-2">
+                     <h1>Activity</h1>
                   </div>
+               </section>
+
+               <div className="userSuggestions border">
+                  <h1>Suggestions</h1>
                </div>
-
-               <div className="userMsgs">
-                  <div className="useMsg">
-                     <UserConversation img={Law} alt='Imagem do usuário Law' name='Law' msg='Mugiwaraaa'/>
-                     <UserConversation img={Kaido} alt='Imagem do usuário Kaido' name='Kaido' msg='Boro Breath'/>
-                     <UserConversation img={Roger} alt='Imagem do usuário Roger' name='Roger' msg='Kamusari'/>
-                  </div>
-                  <div className="useMsg"></div>
-                  <div className="useMsg"></div>
-               </div>
-            </section>
-
-            <section className="sectionMsg flex border">
-               <div className="noMsg flex column">
-                  <span className="flex">
-                     <img src={ SharePosts } alt="Posts" className="sharePosts"/>
-                  </span>
-
-                  <h3 className="mt-2">Your Messages</h3>
-
-                  <p className="mt-1">Send private photos and messages to a friend or group.</p>
-
-                  <Link to="/" className="sendMessage mt-3">
-                     Send Message
-                  </Link>
-               </div>
-            </section>
-         </main>
+            </main>
+         </div>
       </>
    )
 }
